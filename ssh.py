62ffc8a6d7f1467c8e75487fc1c6bdced5931c2b
Ryan <rjlafazan@gmail.com>
2018-09-14 01:45:56 -0700

    working without paramiko... paramiko install issues

--- ssh.py
+++ ssh.py
@@ -1,4 +1,4 @@
-import paramiko, sys, os, socket
+import  paramiko, sys, os, socket
 global host, username, line, input_file
 
 line = "\n-------------------------------------------------------\n"
@@ -12,9 +12,11 @@ try:
     username = raw_input("[*] Enter SSH Username: ")
     input_file = raw_input("[*] Enter SSH Password File: ")
 
+    #check if file exists. If it doesn't, print as follows.
     if os.path.exists(input_file) == False:
         print "\n[*] File Path Does Not Exist !!!"
         sys.exit(4)
 except KeyboardInterrupt:
     print "\n\n[*] User Requested An Interrupt"
     sys.exit(3)
+
