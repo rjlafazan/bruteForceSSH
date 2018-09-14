b9739122a98fb29a53599b72291f17da511a6ca4
Ryan <rjlafazan@gmail.com>
2018-09-14 00:39:21 -0700

    syntax fix

--- ssh.py
+++ ssh.py
@@ -2,10 +2,14 @@ import paramiko, sys, os, socket
 
 line = "\n-------------------------------------------------------\n"
 
+#collect program information
 try: 
-    host = raw_input{"[*] Enter Target Host Address: "}
-    username = raw_input{"[*] Enter SSH Username: "}
-    input_file = raw_input{"[*] Enter SSH Password File: "}
+    #hold target address
+    #hold ssh username to be bruteforced
+    #hold file path string
+    host = raw_input("[*] Enter Target Host Address: ")
+    username = raw_input("[*] Enter SSH Username: ")
+    input_file = raw_input("[*] Enter SSH Password File: ")
 
     if os.path.exists(input_file) == False:
         print "\n[*] File Path Does Not Exist !!!"
